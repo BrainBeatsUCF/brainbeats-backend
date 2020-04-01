e82404dd0d02da7431c4c1f0f9a442d26a32eafa
Restyled.io <commits@restyled.io>
2020-04-01 22:03:56 +0000

    Restyled by clang-format

--- brainbeats-backend/Controllers/Sample.cs
+++ brainbeats-backend/Controllers/Sample.cs
@@ -3,44 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace brainbeats_backend.Controllers
-{
-public class Sample
-{
-    public string id
-    {
-        get;
-        set;
+namespace brainbeats_backend.Controllers {
+  public class Sample {
+    public string id {
+      get;
+      set;
     }
 
-    public string authorId
-    {
-        get;
-        set;
+    public string authorId {
+      get;
+      set;
     }
 
-    public string name
-    {
-        get;
-        set;
+    public string name {
+      get;
+      set;
     }
 
-    public bool? isPrivate
-    {
-        get;
-        set;
+    public bool ? isPrivate {
+      get;
+      set;
     }
 
-    public string file
-    {
-        get;
-        set;
+    public string file {
+      get;
+      set;
     }
 
-    public string type
-    {
-        get;
-        set;
+    public string type {
+      get;
+      set;
     }
-}
+  }
 }
