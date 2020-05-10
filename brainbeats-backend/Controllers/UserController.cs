16ded975639e6a79c716ad52c484a89ddbfeddfa
Justin <justin.c.bang@gmail.com>
2020-05-09 21:36:48 -0400

    User Controller boilerplate WIP

---
+++ brainbeats-backend/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace brainbeats_backend.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class UserController : ControllerBase
+  {
+    private readonly IConfiguration _configuration;
+
+    public UserController(IConfiguration configuration) {
+      _configuration = configuration;
+    }
+
+    [HttpPost]
+    [Route("create")]
+    public string CreateUser(dynamic req) {
+      var body = JsonConvert.DeserializeObject<dynamic>(req.ToString());
+
+      return new string($"{body}");
+    }
+  }
+}
