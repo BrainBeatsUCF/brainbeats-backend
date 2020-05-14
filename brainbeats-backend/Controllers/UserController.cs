2c1910fc20e4e4c00a1aae770c2be1cebb0480b1
Restyled.io <commits@restyled.io>
2020-05-14 08:47:34 +0000

    Restyled by astyle

--- brainbeats-backend/Controllers/UserController.cs
+++ brainbeats-backend/Controllers/UserController.cs
@@ -12,110 +12,110 @@ using Newtonsoft.Json;
 
 namespace brainbeats_backend.Controllers
 {
-  [Route("api/[controller]")]
-  [ApiController]
-  public class UserController : ControllerBase
-  {
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
     [HttpPost]
     [Route("create")]
     public async Task<IActionResult> CreateUser(dynamic req) {
-      var body = JsonConvert.DeserializeObject<dynamic>(req.ToString());
+        var body = JsonConvert.DeserializeObject<dynamic>(req.ToString());
 
-      string firstName = body.firstName;
-      string lastName = body.lastName;
-      string email = body.email;
+        string firstName = body.firstName;
+        string lastName = body.lastName;
+        string email = body.email;
 
-      if (firstName == null || lastName == null || email == null) {
-        return BadRequest("Malformed Request");
-      }
+        if (firstName == null || lastName == null || email == null) {
+            return BadRequest("Malformed Request");
+        }
 
-      string queryString = $"g.addV('user')" +
-        $".property('id', '{email}')" +
-        $".property('firstName', '{firstName}')" +
-        $".property('lastName', '{lastName}')" +
-        $".property('type', 'user')";
+        string queryString = $"g.addV('user')" +
+                             $".property('id', '{email}')" +
+                             $".property('firstName', '{firstName}')" +
+                             $".property('lastName', '{lastName}')" +
+                             $".property('type', 'user')";
 
-      try {
-        var result = await DatabaseConnection.Instance.ExecuteQuery(queryString);
-        return Ok(result);
-      } catch {
-        return BadRequest();
-      }
+        try {
+            var result = await DatabaseConnection.Instance.ExecuteQuery(queryString);
+            return Ok(result);
+        } catch {
+            return BadRequest();
+        }
     }
 
     [HttpPost]
     [Route("read")]
     public async Task<IActionResult> ReadUser(dynamic req) {
-      var body = JsonConvert.DeserializeObject<dynamic>(req.ToString());
+        var body = JsonConvert.DeserializeObject<dynamic>(req.ToString());
 
-      string email = body.email;
+        string email = body.email;
 
-      if (email == null) {
-        return BadRequest("Malformed Request");
-      }
+        if (email == null) {
+            return BadRequest("Malformed Request");
+        }
 
-      string queryString = $"g.V('{email}')";
+        string queryString = $"g.V('{email}')";
 
-      try {
-        var result = await DatabaseConnection.Instance.ExecuteQuery(queryString);
-        return Ok(result);
-      } catch {
-        return BadRequest();
-      }
+        try {
+            var result = await DatabaseConnection.Instance.ExecuteQuery(queryString);
+            return Ok(result);
+        } catch {
+            return BadRequest();
+        }
     }
 
     [HttpPost]
     [Route("update")]
     public async Task<IActionResult> UpdateUser(dynamic req) {
-      var body = JsonConvert.DeserializeObject<dynamic>(req.ToString());
+        var body = JsonConvert.DeserializeObject<dynamic>(req.ToString());
 
-      string firstName = body.firstName;
-      string lastName = body.lastName;
-      string email = body.email;
+        string firstName = body.firstName;
+        string lastName = body.lastName;
+        string email = body.email;
 
-      if ((firstName == null && lastName == null) || email == null) {
-        return BadRequest("Malformed Request");
-      }
+        if ((firstName == null && lastName == null) || email == null) {
+            return BadRequest("Malformed Request");
+        }
 
-      string queryString = $"g.V('{email}')";
-      StringBuilder sb = new StringBuilder(queryString);
+        string queryString = $"g.V('{email}')";
+        StringBuilder sb = new StringBuilder(queryString);
 
-      if (firstName != null) {
-        sb.Append($".property('firstName', '{firstName}')");
-      }
+        if (firstName != null) {
+            sb.Append($".property('firstName', '{firstName}')");
+        }
 
-      if (lastName != null) {
-        sb.Append($".property('lastName', '{lastName}')");
-      }
+        if (lastName != null) {
+            sb.Append($".property('lastName', '{lastName}')");
+        }
 
-      Console.WriteLine(sb.ToString());
+        Console.WriteLine(sb.ToString());
 
-      try {
-        var result = await DatabaseConnection.Instance.ExecuteQuery(sb.ToString());
-        return Ok(result);
-      } catch {
-        return BadRequest();
-      }
+        try {
+            var result = await DatabaseConnection.Instance.ExecuteQuery(sb.ToString());
+            return Ok(result);
+        } catch {
+            return BadRequest();
+        }
     }
 
     [HttpPost]
     [Route("delete")]
     public async Task<IActionResult> DeleteUser(dynamic req) {
-      var body = JsonConvert.DeserializeObject<dynamic>(req.ToString());
-      string email = body.email;
+        var body = JsonConvert.DeserializeObject<dynamic>(req.ToString());
+        string email = body.email;
 
-      if (email == null) {
-        return BadRequest("Malformed Request");
-      }
+        if (email == null) {
+            return BadRequest("Malformed Request");
+        }
 
-      string queryString = $"g.V('{email}').drop()";
+        string queryString = $"g.V('{email}').drop()";
 
-      try {
-        var result = await DatabaseConnection.Instance.ExecuteQuery(queryString);
-        return Ok(result);
-      } catch {
-        return BadRequest();
-      }
+        try {
+            var result = await DatabaseConnection.Instance.ExecuteQuery(queryString);
+            return Ok(result);
+        } catch {
+            return BadRequest();
+        }
     }
-  }
+}
 }
